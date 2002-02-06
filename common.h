ec4e82cb933e3dcf13b97f941f566834eb7f199d
fenner <fenner@dd8174f5-eb3e-0410-b25e-edeba5481af1>
2002-02-06 18:42:12 +0000

    rule/object data structures.

--- common.h
+++ common.h
@@ -3,3 +3,64 @@ struct range {
 	int lo;
 	int hi;
 };
+
+struct rule {
+	char *name;
+	int line;
+	struct object *rule;
+	struct rule *next;
+};
+
+/*
+ * Types:
+ * - Alternation
+ *   - left + right
+ * - Rule
+ *   - repetition
+ *   - concatenation
+ * - Terminal string
+ *   - case sensitive or not
+ *   - repetition
+ *   - concatenation
+ * - Terminal character range
+ *   - repetition
+ *   - concatenation
+ */
+#define	T_ALTERNATION	1
+#define	T_RULE		2
+#define	T_GROUP		3
+#define	T_TERMSTR	4
+#define	T_TERMRANGE	5
+#define	T_PROSE		6
+
+typedef struct object {
+	int type;
+	struct object *next;
+	union {
+		struct {
+			struct object *left;
+			struct object *right;
+		} alternation;
+		struct {
+			struct range repetition;
+			union {
+			    struct {
+				    char *name;	/* for forward ref. */
+				    struct rule *rule;
+			    } rule;
+			    struct object *group;
+			    struct {
+				    char *str;
+				    int flags;
+			    } termstr;
+			    struct {
+				    char lo;
+				    char hi;
+			    } termrange;
+			} e;
+		} e;
+		char *proseval;
+	} u;
+} object;
+
+#define	F_CASESENSITIVE		1	/* termstr.str is case sensitive */
